f37496b6cdb06dde7804dbcded2c4aed0bfa4e3c
Neil Penning <neil.penning@gmail.com>
2021-07-06 08:00:02 -0400

    tmux status bar

--- first_run.sh
+++ first_run.sh
@@ -1,3 +1,6 @@
 chsh -s /bin/bash # changes default shell to bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# installs tmux-plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
