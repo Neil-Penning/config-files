38bd841a6ffaeff057cca05c898519e9c3b74cea
Neil Penning <neil.penning@gmail.com>
2022-04-25 02:37:59 -0400

    display what files are copied

--- cp-config-files.sh
+++ cp-config-files.sh
@@ -1,43 +1,55 @@
+vcp () {
+    if [[ -d "$1" ]] ; then
+        echo "Cannot copy the directory \"$1\"";
+    elif [[ -d "$2" ]] ; then
+        echo "Cannot copy to the dirctory \"$2\""
+    elif ! cmp --silent "$1" "$2" ; then 
+        echo "Copying: \"$1\" -> \"$2\""
+        cp "$1" "$2"
+#    else
+#        echo "Not copies : \"$1\" -> \"$2\""
+    fi
+}
 if ! [ -f ./os.txt ]; 
 then
     echo "FAILED: set OS type in $HOME/config-files/os.txt";
     exit 1;
 else
-    cp ./os.txt $HOME/.os.txt
+    vcp ./os.txt $HOME/.os.txt
 fi
 
 #Bash files
-    cp bash_aliases.sh ~/.bash_aliases
-    cp bash_profile.sh ~/.bash_profile
+    vcp bash_aliases.sh ~/.bash_aliases
+    vcp bash_profile.sh ~/.bash_profile
 
 # Sage
     [ -d $HOME/.sage/ ] || (mkdir -p $HOME/.sage/; echo "made directory $HOME/.sage")
-    cp init.sage ~/.sage/init.sage
+    vcp init.sage ~/.sage/init.sage
     [ -d $HOME/.sage/ipython-5.0.0/profile_default/ ] || (mkdir -p $HOME/.sage/ipython-5.0.0/profile_default/; echo "made directory $HOME/.sage/ipython-5.0.0/profile_default/")
-    cp ./ipython_config.py $HOME/.sage/ipython-5.0.0/profile_default/
+    vcp ./ipython_config.py $HOME/.sage/ipython-5.0.0/profile_default/ipython_config.py
 
 # Vim 
-    cp vimrc ~/.vimrc
+    vcp vimrc ~/.vimrc
     [ -d $HOME/.vim/ftplugin ] || (mkdir -p $HOME/.vim/ftplugin; echo "made directory $HOME/.vim/ftplugin")
-    cp ./filetype.vim $HOME/.vim/
-    cp ./vim_filetype_plugins/* ~/.vim/ftplugin/
+    vcp ./filetype.vim $HOME/.vim/filetype.vim
+    vcp ./vim_filetype_plugins/* ~/.vim/ftplugin/
     # make vim syntax directory if it doesn't exist
     [ -d $HOME/.vim/syntax ] || (mkdir -p $HOME/.vim/syntax; echo "made directory $HOME/.vim/syntax")
     # copy syntax file from vim-tex-syntax if submodule is included
     if [ -e ./vim-tex-syntax/syntax/tex.vim ];
     then
-        cp ./vim-tex-syntax/syntax/tex.vim $HOME/.vim/syntax;
+        vcp ./vim-tex-syntax/syntax/tex.vim $HOME/.vim/syntax/tex.vim;
     else
         echo "./vim-tex-syntax does not exist"
     fi
 
 
 # Tmux
-    cp tmux.conf ~/.tmux.conf
+    vcp tmux.conf ~/.tmux.conf
     ~/.tmux/plugins/tpm/bin/install_plugins
 
 if ! [ -f $HOME/.ssh/config ]; 
 then
     echo "Copying ssh config file";
-    cp ssh_config "$HOME/.ssh/config"
+    vcp ssh_config "$HOME/.ssh/config"
 fi
