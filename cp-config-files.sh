1dee0e311dfbb24790e59b2f066b3d1d4b4564da
Neil Penning <neil.penning@gmail.com>
2022-12-26 13:17:37 -0500

    dynamic colors and better indentation for cp-config-files

--- cp-config-files.sh
+++ cp-config-files.sh
@@ -15,6 +15,10 @@ print_create() { print_color $COLOR_CREATE "$1"; }
 print_bad() { print_color $COLOR_BAD "$1"; }
 print_good() { print_color $COLOR_GOOD "$1"; }
 print_occur() { print_color $COLOR_OCCUR "$1"; }
+
+print_dne() { print_bad "Does not exist: \"${NC}$1${COLOR_BAD}\""; }
+print_no_overwrite() { print_good "No Overwrite: ${NC}$1${COLOR_GOOD}"; }
+
 vmkdir () {
     if ! [[ -d "$1" ]]; then
         mkdir -p "$1"
@@ -23,9 +27,9 @@ vmkdir () {
 }
 vcp () {
     if [[ -d "$1" ]] ; then
-        print_bad "Cannot copy the directory ${NC}\"$1\""
+        print_bad "Cannot copy the directory \"${NC}$1${COLOR_BAD}\""
     elif [[ -d "$2" ]] ; then
-        print_bad "Cannot copy to the dirctory ${NC}\"$2\""
+        print_bad "Cannot copy to the directory \"${NC}$2${COLOR_BAD}\""
     elif ! [[ -e "$2" ]] ; then 
         print_create "Creating: \"${NC}$1${COLOR_CREATE}\" -> \"${NC}$2${COLOR_CREATE}\""
         cp "$1" "$2"
@@ -45,7 +49,7 @@ fi
 
 #Bash files
     if [[ -e  $HOME/.bash_prompt ]]; then
-        print_good "No Overwrite: ${NC}$HOME/.bash_prompt"
+        print_no_overwrite "$HOME/.bash_prompt"
     else
         vcp ./prompts/default/bash_prompt $HOME/.bash_prompt
     fi
@@ -61,33 +65,31 @@ fi
 # Vim 
     vcp vimrc ~/.vimrc
     vmkdir "$HOME/.vim/"
-    vmkdir "$HOME/.vim/ftplugin/"
-    vcp ./vim/filetype.vim $HOME/.vim/filetype.vim
-
-    # TODO make vcp support folder copying
 
     #ftplugin
-    vcp ./vim/ftplugin/python.vim   ~/.vim/ftplugin/python.vim
-    vcp ./vim/ftplugin/tex.vim      ~/.vim/ftplugin/tex.vim
-    vcp ./vim/ftplugin/java.vim     ~/.vim/ftplugin/java.vim
-    vcp ./vim/ftplugin/sh.vim       ~/.vim/ftplugin/sh.vim
-    vcp ./vim/ftplugin/vim.vim      ~/.vim/ftplugin/vim.vim
+        vmkdir "$HOME/.vim/ftplugin/"
+        vcp ./vim/filetype.vim $HOME/.vim/filetype.vim
+        vcp ./vim/ftplugin/python.vim   ~/.vim/ftplugin/python.vim
+        vcp ./vim/ftplugin/tex.vim      ~/.vim/ftplugin/tex.vim
+        vcp ./vim/ftplugin/java.vim     ~/.vim/ftplugin/java.vim
+        vcp ./vim/ftplugin/sh.vim       ~/.vim/ftplugin/sh.vim
+        vcp ./vim/ftplugin/vim.vim      ~/.vim/ftplugin/vim.vim
     #syntax
-    # make vim syntax directory if it doesn't exist
-    vmkdir "$HOME/.vim/syntax/"
-    vcp ./vim/syntax/scene.vim    ~/.vim/syntax/scene.vim
-    vcp ./vim/syntax/quote.vim    ~/.vim/syntax/quote.vim
+        # make vim syntax directory if it doesn't exist
+        vmkdir "$HOME/.vim/syntax/"
+        vcp ./vim/syntax/scene.vim    ~/.vim/syntax/scene.vim
+        vcp ./vim/syntax/quote.vim    ~/.vim/syntax/quote.vim
 
     # copy syntax file from vim-tex-syntax if submodule is included
     if [[ -e ./vim/vim-tex-syntax/syntax/tex.vim ]]; then
         vcp ./vim/vim-tex-syntax/syntax/tex.vim $HOME/.vim/syntax/tex.vim
     else
-        print_bad "./vim/vim-tex-syntax does not exist"
+        print_dne "\$CONFIG_FILES/vim/vim-tex-syntax/"
     fi
 
 # SSH
     if [[ -f $HOME/.ssh/config ]]; then
-        print_good "No Overwrite: ${NC}$HOME/.ssh/config"
+        print_no_overwrite "$HOME/.ssh/config"
     else
         print_occur "Copying ssh config file";
         vcp ssh_config "$HOME/.ssh/config"
@@ -96,7 +98,7 @@ fi
 # Tmux
     vcp tmux.conf $HOME/.tmux.conf
     if [[ -e  $HOME/.tmux_profile.conf ]]; then
-        print_good "No Overwrite: ${NC}$HOME/.tmux_profile.conf"
+        print_no_overwrite "$HOME/.tmux_profile.conf"
     else
         vcp ./prompts/default/tmux_profile.conf $HOME/.tmux_profile.conf
     fi
